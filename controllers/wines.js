0fa49ff805f15e23afae158f301e9c7ceaa48213
jessaclark <jemahold@icloud.com>
2021-07-20 19:17:03 -0400

    setup backend full CRUD with controllers and routes

--- controllers/wines.js
+++ controllers/wines.js
@@ -1,11 +1,64 @@
-import Wine from '../models/wine.js'
+import Wine from "../models/wine.js";
 
 export const getWines = async (req, res) => {
   try {
-    const wine = await Wine.find()
-    res.json(wine) 
-  } catch (error){
-    res.status(500).send(error.message)
+    const wines = await Wine.find();
+    res.json(wines);
+  } catch (error) {
+    res.status(500).send(error.message);
   }
-}
+};
 
+export const getOneWine = async (req, res) => {
+  try {
+    const wine = await Wine.findById(req.params.id);
+    res.json(wine);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+export const addWine = async (req, res) => {
+  try {
+    const wine = new Wine(req.body);
+    await wine.save();
+    res.status(201).json(wine);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateWine = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Wine.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true },
+      (err, wine) => {
+        if (err) {
+          res.status(500).json(err);
+        }
+        if (!wine) {
+          res.status(500).send("Wine not found!");
+        }
+        return res.status(200).json(wine);
+      }
+    );
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+export const deleteWine = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Wine.findByIdAndUpdate(id);
+    if (deleted) {
+      return res.status(200).send("Wine deleted!");
+    }
+    throw new Error("Wine not found!");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
--- routes/wines.js
+++ routes/wines.js
@@ -5,6 +5,10 @@ import * as controllers from '../controllers/wines.js'
 const router = Router();
 
 router.get('/wines', controllers.getWines)
+router.get('/wines/:id', controllers.getOneWine)
+router.post('/wines', controllers.addWine)
+router.put('/wines/:id', controllers.updateWine)
+router.delete('/wines/:id', controllers.deleteWine)
 
 export default router;
 
