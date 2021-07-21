09ad39094972274eb630c72ea3709ca7bd8fb6fd
jessaclark <jemahold@icloud.com>
2021-07-21 15:10:46 -0400

    setup user services

---
+++ client/src/services/users.js
@@ -0,0 +1,30 @@
+import api from "./apiConfig"
+import jwtDecode from "jwt-decode"
+export const signUp = async (credentials) => {
+    try {
+        const res = await api.post("/sign-up", credentials)
+        localStorage.setItem( "token", res.data.token)
+        const user = jwtDecode(res.data.token)
+        return user
+    } catch (error) {
+        throw error
+    }
+}
+export const signIn = async (credentials) => {
+    try {
+        const res = await api.post("/sign-in", credentials)
+        localStorage.setItem( "token", res.data.token)
+        const user = jwtDecode(res.data.token)
+        return user
+    } catch (error) {
+        throw error
+    }
+}
+export const signOut = async () => {
+    try {
+        localStorage.removeItem( "token")
+        return true
+    } catch (error) {
+        throw error
+    }
+}
