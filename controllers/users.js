b8c910dbc0d14f4450ec4a11c1dcb2eaf1acda0c
carlsonmr <carlsonmr000@gmail.com>
2021-07-21 14:52:47 -0500

    add verify function to backend and a verify route

--- controllers/users.js
+++ controllers/users.js
@@ -83,3 +83,17 @@ export const signIn = async (req, res) => {
     res.status(500).json({ error: error.message })
   }
 };
+
+
+export const verify = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1]
+    const payload = jwt.verify(token, TOKEN_KEY)
+    if (payload) {
+      res.json(payload)
+    }
+  } catch (error) {
+    console.log(error.message)
+    res.status(401).send("Not authorized")
+  }
+}
