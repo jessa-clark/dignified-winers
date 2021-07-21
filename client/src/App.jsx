7e88c8401223a964f57733b451963efa22d0fb34
carlsonmr <carlsonmr000@gmail.com>
2021-07-21 15:10:16 -0500

    added imports to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import { useState, useEffect } from "react";
+// import { verifyUser } from "./services/users"
 
+//components
+import AllWines from "./screens/AllWines/AllWines"
+import Home from "./screens/Home/Home"
+import SignIn from "./screens/SignIn/SignIn"
+import SignOut from "./screens/SignOut/SignOut"
+import SignUp from "./screens/SignUp/SignUp"
+import WineAdd from "./screens/WineAdd/WineAdd"
+import WineDetail from "./screens/WineDetail/WineDetail"
+import WineEdit from "./screens/WineEdit/WineEdit"
 import './App.css';
+
 
 function App() {
   return (
