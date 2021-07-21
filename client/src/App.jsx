09acddba0fdf2c690597e305cec8f3b2810bc7ef
carlsonmr <carlsonmr000@gmail.com>
2021-07-21 15:29:29 -0500

    added routes to app.jsx add user state, create fetch user function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,66 @@
 import { Route, Switch, Redirect } from "react-router-dom";
 import { useState, useEffect } from "react";
-// import { verifyUser } from "./services/users"
+import { verifyUser } from "./services/users";
 
 //components
-import AllWines from "./screens/AllWines/AllWines"
-import Home from "./screens/Home/Home"
-import SignIn from "./screens/SignIn/SignIn"
-import SignOut from "./screens/SignOut/SignOut"
-import SignUp from "./screens/SignUp/SignUp"
-import WineAdd from "./screens/WineAdd/WineAdd"
-import WineDetail from "./screens/WineDetail/WineDetail"
-import WineEdit from "./screens/WineEdit/WineEdit"
-import './App.css';
+import AllWines from "./screens/AllWines/AllWines";
+import Home from "./screens/Home/Home";
+import SignIn from "./screens/SignIn/SignIn";
+import SignOut from "./screens/SignOut/SignOut";
+import SignUp from "./screens/SignUp/SignUp";
+import WineAdd from "./screens/WineAdd/WineAdd";
+import WineDetail from "./screens/WineDetail/WineDetail";
+import WineEdit from "./screens/WineEdit/WineEdit";
+import "./App.css";
 
+const App = () => {
+  const [user, setUser] = useState(null);
 
-function App() {
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await verifyUser();
+      setUser(user ? user : null);
+    };
+    fetchUser();
+  }, []);
+
   return (
     <div className="App">
-      
+      <Switch>
+        <Route exact path="/">
+          <Home user={user} />
+        </Route>
+
+        <Route exact path="/sign-up">
+          <SignUp setUser={setUser} />
+        </Route>
+
+        <Route exact path="/sign-out">
+          <SignOut setUser={setUser} />
+        </Route>
+
+        <Route exact path="/sign-in">
+          <SignIn setUser={setUser} />
+        </Route>
+
+        <Route exact path="/wines">
+          <AllWines user={user} />
+        </Route>
+
+        <Route exact path="/edit/:id">
+          {user ? <WineEdit user={user} /> : <Redirect to="/sign-up" />}
+        </Route>
+
+        <Route exact path="/add-wine">
+          {user ? <WineAdd user={user} /> : <Redirect to="/sign-up" />}
+        </Route>
+
+        <Route exact path="/wines/:id">
+          <WineDetail user={user} />
+        </Route>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
