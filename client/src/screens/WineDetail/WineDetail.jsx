52daddf52bec2a4dc8856f07632c2037e73f59dc
carlsonmr <carlsonmr000@gmail.com>
2021-07-22 15:42:23 -0500

    format wine detail card format edit and delete buttons format related wines cards

--- client/src/screens/WineDetail/WineDetail.jsx
+++ client/src/screens/WineDetail/WineDetail.jsx
@@ -52,23 +52,23 @@ const WineDetail = (props) => {
           alt={wine.name}
         />
         <div className='detail'>
-          <div className='name'>{wine.name}</div>
-          <div className='vineyard'>{`${wine.vineyard} Vineyard`}</div>
-          <div className='year'>{wine.year}</div>
-          <div className='description'>{wine.description}</div>
-           <div className='button-container'>
-            <Link className='edit-button' to={`/wines/edit/${id}`}>
+          <div className='wine-detail-name'>{wine.name}</div>
+          <div className='wine-detail-vineyard'>{`${wine.vineyard} Vineyard`}</div>
+          <div className='wine-detail-year'>{wine.year}</div>
+          <div className='wine-detail-description'>{wine.description}</div>
+           <div className='wine-detail-button-container'>
+            <Link className='wine-detail-edit-button' to={`/wines/edit/${id}`}>
               Edit
             </Link>
             <button
-              className='delete-button'
+              className='wine-detail-delete-button'
               onClick={handleSubmit}>
               Delete
             </button>
           </div>
           <div>
             <h1>Related Wines</h1>
-            <div className="all-wines">
+            <div className="all-wines-detail">
         {wines?.length ? (
           [...wines].splice(0,4).map((wine) => <Wine key={wine._id} wine={wine} />)
         ) : (
