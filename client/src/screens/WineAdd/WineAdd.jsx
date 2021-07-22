01872b6d8062a9a42b7581a0b016cff349640810
yeezick <ramerick5@gmail.com>
2021-07-22 12:44:34 -0400

    MAJOR ROUTER FIX allows user state to persist on reload addWine + editWine need verified user token in order to access screen, otherwise is redirected to sign-up beginning footer styling

--- client/src/screens/WineAdd/WineAdd.jsx
+++ client/src/screens/WineAdd/WineAdd.jsx
@@ -1,109 +1,121 @@
-import { useState } from 'react'
-import './WineAdd.css'
+import { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
 import Layout from "../../components/Layout/Layout";
-import { Redirect } from 'react-router-dom'
-import { addWine } from '../../services/wines'
+import { verify } from "../../services/users";
+import { addWine } from "../../services/wines";
+import "./WineAdd.css";
 
 const WineAdd = (props) => {
   const [wine, setWine] = useState({
-    name: '',
-    vineyard: '',
-    year: '',
-    imgURL: '',
-    description: '',
-    type: '',
-  })
+    name: "",
+    vineyard: "",
+    year: "",
+    imgURL: "",
+    description: "",
+    type: "",
+  });
 
-  const [isCreated, setCreated] = useState(false)
+  const [isCreated, setCreated] = useState(false);
+  const [userBool, setUserBool] = useState(null);
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const userExists = await verify();
+      setUserBool(userExists ? true : false);
+    };
+    checkUser();
+  }, []);
 
   const handleChange = (event) => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
     setWine({
       ...wine,
       [name]: value,
-    })
-  }
+    });
+  };
 
   const handleSubmit = async (event) => {
-    event.preventDefault()
-    const created = await addWine(wine)
-    setCreated( {created} )
-  }
+    event.preventDefault();
+    const created = await addWine(wine);
+    setCreated({ created });
+  };
 
   if (isCreated) {
-    return <Redirect to={`/wines`} />
+    return <Redirect to={`/wines`} />;
   }
 
-  return (
+  return !userBool && userBool !== null ? (
+    <Redirect to="/sign-up" />
+  ) : (
     <Layout user={props.user}>
-      <form className='create-form' onSubmit={handleSubmit}>
+      <form className="create-form" onSubmit={handleSubmit}>
         <input
-        className='input-name'
-        placeholder='Wine Name'
-        value={wine.name}
-        name='name'
-        required
-        autoFocus
-        onChange={handleChange}
+          className="input-name"
+          placeholder="Wine Name"
+          value={wine.name}
+          name="name"
+          required
+          autoFocus
+          onChange={handleChange}
         />
         <input
-        className='input-vineyard'
-        placeholder='Vineyard Name'
-        value={wine.vineyard}
-        name='vineyard'
-        required
-        autoFocus
-        onChange={handleChange}
+          className="input-vineyard"
+          placeholder="Vineyard Name"
+          value={wine.vineyard}
+          name="vineyard"
+          required
+          autoFocus
+          onChange={handleChange}
         />
         <input
-        className='input-year'
-        placeholder='Year'
-        value={wine.year}
-        name='year'
-        required
-        autoFocus
-        onChange={handleChange}
+          className="input-year"
+          placeholder="Year"
+          value={wine.year}
+          name="year"
+          required
+          autoFocus
+          onChange={handleChange}
         />
         <img className="wine-image" src={wine.imgURL} alt={wine.name} />
         <input
-        className='input-image-link'
-        placeholder='Image Link'
-        value={wine.imgURL}
-        name='imgURL'
-        required
-        autoFocus
-        onChange={handleChange}
+          className="input-image-link"
+          placeholder="Image Link"
+          value={wine.imgURL}
+          name="imgURL"
+          required
+          autoFocus
+          onChange={handleChange}
         />
         <input
-        className='textarea-description'
-        rows={10}
-        placeholder='Description'
-        value={wine.description}
-        name='description'
-        required
-        autoFocus
-        onChange={handleChange}
+          className="textarea-description"
+          rows={10}
+          placeholder="Description"
+          value={wine.description}
+          name="description"
+          required
+          autoFocus
+          onChange={handleChange}
         />
         <select
-        className='dropdown-type'
-        placeholder='Type'
-        value={wine.type}
-        name='type'
-        required
-        autoFocus
-        onChange={handleChange}
+          className="dropdown-type"
+          placeholder="Type"
+          value={wine.type}
+          name="type"
+          required
+          autoFocus
+          onChange={handleChange}
         >
           <option value="dropdown">Select Type</option>
           <option value="red">red</option>
           <option value="white">white</option>
           <option value="specialty">specialty</option>
         </select>
-        <button type='submit' className='add-button'>
+        <button type="submit" className="add-button">
           Add Wine
         </button>
       </form>
     </Layout>
-  )
-}
+  );
+};
 
-export default WineAdd
+export default WineAdd;
