6138fd21bd10438a42dce811f3ce9374a1aa7821
yeezick <ramerick5@gmail.com>
2021-07-22 15:16:34 -0400

    inserts skeleton text in landing-section as i begin to develop it

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,12 +1,21 @@
-import Layout from  "../../components/Layout/Layout";
+import { useHistory } from "react";
+import Layout from "../../components/Layout/Layout";
 import "./Home.css";
 
 const Home = () => {
-  return <Layout>
-    <section className="home-container">
-      <h2>hello world</h2>
-    </section>
-  </Layout>;
+  const takeToWines = () => {};
+  return (
+    <Layout>
+      <section className="landing-section">
+        <div className="landing-greeting">
+          <h2 className="landing-text">Welcome to the World of Wine</h2>
+          <button onClick={takeToWines} className="landing-button">
+            Browse our Wines
+          </button>
+        </div>
+      </section>
+    </Layout>
+  );
 };
 
 export default Home;
