42cc54b99440f8462a10b03ec5a36051dbf2e01c
rahulpjo <rjoshi9000@gmail.com>
2021-07-26 11:46:54 -0700

    Add routes and controllers for getting and creating user wines

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,9 @@ const router = Router();
 //use imported controllers to perform different stages of authentication
 router.post("/sign-up", controllers.signUp);
 router.post("/sign-in", controllers.signIn);
-router.get("/verify", controllers.verify)
+router.get("/verify", controllers.verify);
 
+router.get("/users/:id/wines", controllers.getUserWines);
+router.get("/users/:id/wines/:wineId", controllers.getUserWine);
 
 export default router;
