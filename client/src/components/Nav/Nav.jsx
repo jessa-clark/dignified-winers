c9a48a946862975e1aacc9e39beb072d80ea4123
carlsonmr <carlsonmr000@gmail.com>
2021-07-27 15:26:02 -0500

    added hamburger menu to mobile screens

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { verify } from "../../services/users";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 // images
 
@@ -8,6 +10,9 @@ import "./Nav.css";
 
 const Nav = () => {
   const [userExists, setUserExists] = useState(null);
+  const [showMenu, setShowMenu] = useState(false);
+
+
   useEffect(() => {
     const checkSigned = async () => {
       const valid = await verify();
@@ -15,8 +20,14 @@ const Nav = () => {
     };
     checkSigned();
   }, []);
+
   return (
-    <nav className="navbar">
+    <header>
+      <FontAwesomeIcon
+        className="fa-bars"
+        icon={faBars}
+        onClick={() => setShowMenu(!showMenu)}
+      />
       <Link to="/" className="navbar-left-container">
         <img
           className="navbar-logo"
@@ -25,19 +36,36 @@ const Nav = () => {
         />
         <h1 className="navbar-title">Dignified Winers</h1>
       </Link>
-      <div className="navbar-right-container">
-        <Link to="/">Home</Link>
-        <Link to="/wines">Browse Wines</Link>
-        {userExists ? (
-          <>
-            <Link to="/add-wine">Add Wine</Link>
-            <Link to="/sign-out">Sign Out</Link>
-          </>
-        ) : (
-          <Link to="/sign-up">Log In/Register</Link>
-        )}
-      </div>
-    </nav>
+      <nav className="navbar">
+        <div className="navbar-right-container">
+          <Link to="/">Home</Link>
+          <Link to="/wines">Browse Wines</Link>
+          {userExists ? (
+            <>
+              <Link to="/add-wine">Add Wine</Link>
+              <Link to="/sign-out">Sign Out</Link>
+            </>
+          ) : (
+            <Link to="/sign-up">Log In/Register</Link>
+          )}
+        </div>
+
+
+        {showMenu ?  <div className="mobile-navbar-right-container">
+          <Link to="/">Home</Link>
+          <Link to="/wines">Browse Wines</Link>
+          {userExists ? (
+            <>
+              <Link to="/add-wine">Add Wine</Link>
+              <Link to="/sign-out">Sign Out</Link>
+            </>
+          ) : (
+            <Link to="/sign-up">Log In/Register</Link>
+          )}
+        </div> : <></> }
+      </nav>
+
+    </header>
   );
 };
 
