585329dcae541e1a703d23d3f19da7459ad47a3e
Jessa Clark <84357702+jessa-clark@users.noreply.github.com>
2021-07-27 15:45:09 -0400

    beginning of featuredwines

--- client/src/components/Review/ReviewForm.jsx
+++ client/src/components/Review/ReviewForm.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import './ReviewForm.css'
 
 const ReviewForm = ({author, rating, description, onChange, onSubmit}) => {
   return (
---
+++ client/src/screens/FeaturedWines/FeaturedWines.jsx
@@ -0,0 +1,15 @@
+import Layout from "../../components/Layout/Layout";
+import { useEffect, useState } from "react";
+import { getWines } from "../../services/wines";
+import Wine from "../../components/Wine/Wine";
+
+const FeaturedWines = () => {
+  const [reviews, setReviews] = useState([]);
+
+
+  return (
+
+  )
+}
+
+export default FeaturedWines
