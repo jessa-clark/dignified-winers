b84fc9f945221f2cc319f7038126fbb5a81ed3c7
rahulpjo <rjoshi9000@gmail.com>
2021-07-27 09:42:06 -0700

    Add rating and review components, and add to details page

--- client/src/components/Wine/Wine.jsx
+++ client/src/components/Wine/Wine.jsx
@@ -2,19 +2,19 @@ import { Link } from "react-router-dom";
 import "./Wine.css";
 
 const Wine = (props) => {
-  const { _id, name, vineyard, imgURL, year } = props.wine;
+  // console.log(props.wine);
   return (
-    <Link className="wine" to={`/wines/${_id}`}>
+    <Link className="wine" to={`/wines/${props.wine._id}`}>
       <article>
         <div className="wine-header">
-          <h3>{name}</h3>
+          <h3>{props.wine.name}</h3>
         </div>
         <div className="wine-body-image">
-          <img src={imgURL} alt={name} />
+          <img src={props.wine.imgURL} alt={props.wine.name} />
         </div>
         <div className="wine-footer">
-          <p>{vineyard}</p>
-          <h5>{year}</h5>
+          <p>{props.wine.vineyard}</p>
+          <h5>{props.wine.year}</h5>
         </div>
       </article>
     </Link>
--- client/src/screens/WineDetail/WineDetail.jsx
+++ client/src/screens/WineDetail/WineDetail.jsx
@@ -4,6 +4,8 @@ import Layout from "../../components/Layout/Layout";
 import { getOneWine, deleteWine, getWines } from "../../services/wines";
 import Wine from "../../components/Wine/Wine";
 import { useParams, Link, useHistory } from "react-router-dom";
+import Rating from "../../components/Rating/Rating";
+import Review from "../../components/Review/Review";
 
 const WineDetail = (props) => {
   const [wine, setWine] = useState(null);
@@ -33,9 +35,7 @@ const WineDetail = (props) => {
     return <h1>Loading ...</h1>;
   }
 
-  
-
-   const handleSubmit = () => {
+  const handleSubmit = () => {
     const deleteOneWine = async () => {
       await deleteWine(id);
       setTimeout(() => {
@@ -48,38 +48,56 @@ const WineDetail = (props) => {
   return (
     <Layout user={props.user}>
       <div className="wine-detail">
-        <div className="wine-detail-image" >
-        <img src={wine.imgURL} alt={wine.name} />
-
+        <div className="wine-detail-image">
+          <img src={wine.imgURL} alt={wine.name} />
         </div>
         <div className="detail">
           <div className="wine-detail-name">{wine.name}</div>
-          <div className="wine-detail-vineyard"> {wine.vineyard.toLowerCase().includes("vineyard") ? wine.vineyard : `${wine.vineyard} Vineyard`}</div>
-          <div className="wine-detail-year">{wine.year}</div>
+          <div className="wine-detail-vineyard">
+            {" "}
+            {wine.vineyard.toLowerCase().includes("vineyard")
+              ? wine.vineyard
+              : `${wine.vineyard} Vineyard`}{" "}
+            • {wine.year}
+          </div>
+          <div className="wine-detail-year">
+            <Rating rating={wine.rating} />
+          </div>
           <div className="wine-detail-description">{wine.description}</div>
           <div className="wine-detail-button-container">
             <Link className="wine-detail-edit-button" to={`/wines/edit/${id}`}>
               Edit
             </Link>
-            <Link className="wine-detail-delete-button" to="/wines" onClick={handleSubmit}>
+            <Link
+              className="wine-detail-delete-button"
+              to="/wines"
+              onClick={handleSubmit}
+            >
               Delete
             </Link>
           </div>
-
         </div>
       </div>
-           <div className="below-line">
-            <h1 className="related">Related Wines</h1>
-            <div className="all-wines-detail">
-              {wines?.length ? (
-                [...wines]
-                  .splice(0, 4)
-                  .map((wine) => <Wine key={wine._id} wine={wine} />)
-              ) : (
-                <h2>Loading...</h2>
-              )}
-            </div>
-          </div>
+      <div className="below-line">
+        <h1 className="related">Reviews</h1>
+        <div className="all-reviews">
+          {wine.reviews.map((review) => (
+            <Review key={review._id} review={review} />
+          ))}
+        </div>
+      </div>
+      <div className="below-line">
+        <h1 className="related">Related Wines</h1>
+        <div className="all-wines-detail">
+          {wines?.length ? (
+            [...wines]
+              .splice(0, 4)
+              .map((wine) => <Wine key={wine._id} wine={wine} />)
+          ) : (
+            <h2>Loading...</h2>
+          )}
+        </div>
+      </div>
     </Layout>
   );
 };
