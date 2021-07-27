2e98dfe435389fb41fb562b9f3f91de2a037818a
yeezick <ramerick5@gmail.com>
2021-07-27 12:42:08 -0400

    creating review form component

---
+++ client/src/components/Review/ReviewForm.jsx
@@ -0,0 +1,34 @@
+
+const ReviewForm = ({author, rating, description, onChange, onSubmit}) => {
+  return (
+    <form onSubmit={(e) => onSubmit(e)}>
+      <input className="add-review-author"
+      placeholder="Author"
+      value={author}
+      name="author"
+      required
+      onChange={(e) => onChange(e)}
+      />
+      <input className="add-review-rating"
+      placeholder="Rating (1-5)"
+      value={rating}
+      name="rating"
+      required
+      onChange={(e) => onChange(e)} />
+      <textarea 
+      className="add-review-description"
+      rows={10}
+      placeholder="You can w(h)ine in here..."
+      value={description}
+      name="description"
+      required
+      onChange={(e)=>onChange(e)}
+      />
+      <button type="submit" className="add-review-submit">
+        Submit
+      </button>
+    </form>
+  )
+}
+
+export default ReviewForm
