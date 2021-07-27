639b9c080c3432d2158e96db2df2e4128fc7f704
Jessa Clark <84357702+jessa-clark@users.noreply.github.com>
2021-07-27 18:51:09 -0400

    add alt to img on feature wine card

--- client/src/components/Featured/FeaturedCard.jsx
+++ client/src/components/Featured/FeaturedCard.jsx
@@ -7,7 +7,7 @@ const FeaturedCard = (props) => {
   const { rating, imgURL, name, description } = props.review;
   return (
     <article className="featured-card">
-      <img src={imgURL} className="featured-card-image"/>
+      <img src={imgURL} alt="featured wine" className="featured-card-image"/>
       <div className="featured-card-text">
       <h3>{`#${props.rank} ${name}`}</h3>
       <Rating className="review-rating" rating={rating} />
